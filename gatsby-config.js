b31f87c0bfb5acf06b2fbc176537cf6440a90e73
Juliette Lamarche <juliette.lamarche@princesse.xyz>
2022-02-24 19:51:21 -0500

    fixed italics

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,7 @@ module.exports = {
       resolve: `gatsby-plugin-pnpm`,
     },
     `gatsby-plugin-emotion`,
+    //` placeholder=none` causes memory leak; see https://github.com/gatsbyjs/gatsby/issues/34795
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
@@ -22,8 +23,9 @@ module.exports = {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
         fonts: [
-          `spectral`,
-          `arimo`
+          `Spectral:400`,
+          `Spectral:ital,400`,
+          `Arimo:400,700`,
         ],
         display: 'block'
       }
